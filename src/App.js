0c4031f1183dfa6acece41baf325e6b593bae6b2
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-02-13 10:14:36 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Voting from './voting';
+import {List, Map} from 'immutable'; 
 
-const pair = ['Trainspotting', '28 Days Later'];
+const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({
+  'Trainspotting': 5,
+  '28 Days Later': 4
+});
+
 class App extends Component {
   render() {
     return (
-        <Voting pair={pair} />
+      <div>
+        {React.cloneElement(this.props.children, {pair, tally})}
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Voting from './voting';
+import Results from './results';
+import {Router, Route, hashHistory} from 'react-router';
 import './index.css';
 
+const routes = 
+<Route component={App}>
+  <Route path="/" component={Voting} />
+  <Route path="/results" component={Results} />
+</Route>;
+
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('root')
 );
